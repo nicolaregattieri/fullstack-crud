c781d6d017c14648435fdd74467c69089d83c401
Nicola Regattieri <nicregattieri@gmail.com>
2021-05-02 19:21:48 -0300

    graphql and graphql resolver working with type-graphl

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,33 @@
-// import { Post } from './entities/Post';
+
 import { __prod__ } from './constants';
+// import { Post } from './entities/Post';
 import { MikroORM } from "@mikro-orm/core";
 import microConfig from "./mikro-orm.config";
+import { ApolloServer } from 'apollo-server-express';
+import express from "express";
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
+
 
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
-  // const post = orm.em.create(Post, {title: 'my first post'});
-  // await orm.em.persistAndFlush(post);
-  // const posts = await orm.em.find(Post,{})
-  // console.log(posts);
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers:[HelloResolver],
+      validate: false,
+    })
+  })
+
+  apolloServer.applyMiddleware({app});
+
+  app.listen(4000, ()=> {
+    console.log('server started on localhost:4000')
+  })
 
 }
 
