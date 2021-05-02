a92b6941a06ef81f6927ea15874efe888d8f46e0
Nicola Regattieri <nicregattieri@gmail.com>
2021-05-02 18:01:36 -0300

    watch then dev created the table

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
+import { Post } from './entities/Post';
+import { __prod__ } from './constants';
+import { MikroORM } from "@mikro-orm/core";
+import microConfig from "./mikro-orm.config";
+
+
+const main = async () => {
+  const orm = await MikroORM.init(microConfig);
+  await orm.getMigrator().up();
+  const post = orm.em.create(Post, {title: 'my first post'});
+  await orm.em.persistAndFlush(post);
+}
+
+main().catch(err=>{
+  console.error(err);
+})
+
+
 console.log("hi man")
